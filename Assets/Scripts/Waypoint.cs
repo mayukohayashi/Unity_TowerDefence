9ae4881fba862ce071a7d072cb84f5bed88ea2ce
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-08-21 14:41:26 +0900

    add: locate balista, find enemy and look to enemy

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject towerPrefab;
     [SerializeField] bool isPlaceable;
 
     void OnMouseDown()
     {
         if(isPlaceable)
         {
-            Debug.Log(transform.name);
+        // Debug.Log(transform.name);
+        Instantiate(towerPrefab, transform.position, Quaternion.identity);
+        isPlaceable = false;
         }
     }
 }
