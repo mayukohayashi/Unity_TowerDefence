b565d17b391d68e4f304c88e0536e7dd1a7cfc61
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-08-23 11:32:52 +0900

    update: coordinate to colored

--- Assets/Scripts/CoordinateLabeler.cs
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -6,12 +6,20 @@ using TMPro;
 [ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockedColor = Color.gray;
+
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
+    Waypoint waypoint;
 
     void Awake()
     {
         label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+
+        waypoint = GetComponentInParent<Waypoint>();
+
         DisplayCoordinates();
     }
 
@@ -22,6 +30,9 @@ public class CoordinateLabeler : MonoBehaviour
             DisplayCoordinates();
             UpdateObjectTitle();
         }
+
+        ColorCoordinates();
+        ToggleLabels();
     }
 
     void DisplayCoordinates()
@@ -40,5 +51,26 @@ public class CoordinateLabeler : MonoBehaviour
     void UpdateObjectTitle()
     {
         transform.parent.name = coordinates.ToString();
+    }
+
+    void ColorCoordinates()
+    {
+        //
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedColor;
+        }
+    }
+
+    void ToggleLabels()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
     }
 }
